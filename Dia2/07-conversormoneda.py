baac1653d47b4af308a9e6d115b9a5782c3cf395
Max LT98 <max.laura95@gmail.com>
2024-12-06 20:52:18 -0500

    clase 3 semana 1

--- Dia2/07-conversormoneda.py
+++ Dia2/07-conversormoneda.py
@@ -1,24 +1,29 @@
+import os
+
 #razon de cambio de dolar a sol
 razon = 3.73
 
 bandera = "si"
 while (bandera =="si"):
-
-    print ("===============CONVERSOR DE DOLARES A SOLES=============")
+    os.system ("clear")
+    print ("===============CONVERSOR DE MONEDA=============")
     
     operacion = int (input (""" Ingrese el tipo de operacion 
                      1) De Soles a Dolares
                      2) de Dolares a Soles
                      """))
     if (operacion == 1):
+        os.system ("clear")
+        print ("===============CONVERSOR DE SOLES A DOLARES=============")
         soles = float ( input("Ingrese el monto en Soles : "))
         cambio = soles / razon
         print(f" El monto en dolares es de {cambio} ")
     elif (operacion ==2):
+        print ("===============CONVERSOR DE DOLARES A SOLES=============")
         dolares = float (input("Ingrese el monto en dolares : "))
         cambio = dolares * razon
         print(f" El monto en soles es de {cambio} ")
     else:
         print (" Esta operacion no existe!!!")
-
+    
     bandera = input ("¿Desea continuar...? (si/no) : ")
