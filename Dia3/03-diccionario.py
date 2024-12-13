b9c8508bf175334aaf8584d5f7c8dce0f88d8fe8
Max LT98 <max.laura95@gmail.com>
2024-12-12 20:38:36 -0500

    dia 5 imagenes

--- Dia3/03-diccionario.py
+++ Dia3/03-diccionario.py
@@ -1,10 +1,21 @@
 capitales = {
-              'Peru' : 'Lima',
-              'Ecuador' : 'Quito',
-              'Chile' : 'Santiago',
-              'Colombia' : 'Bogota'
-            }
+        'Perú':'Lima',
+        'Ecuador':'Quito',
+        'Chile':'Santiago',
+        'Colombia':'Bogota'
+    }
 
-pais = input ("ingrese el pais ")
-capital = capitales.get (pais)
-print (f"La capital de {pais} es {capital}")
+#print(capitales['Chile'])
+pais = input('Ingrese el pais :')
+if pais in capitales:
+    capital = capitales.get(pais)
+    print(f'la capital de {pais} es {capital}')
+    eliminar_capital = input('¿Desea eliminar la capital?(si,no)')
+    if eliminar_capital == 'si':
+        capitales.pop(pais,'NO EXISTE')
+        print(capitales)
+else:
+    print(f'No se encontro la capital de {pais}')
+    nueva_capital = input(f'Ingrese capital de {pais} :')
+    capitales.update({pais:nueva_capital})
+    print(capitales)
