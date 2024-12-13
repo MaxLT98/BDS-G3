f4bc94c94f47f5bfcd47985603b84e08db2c2bf5
Max LT98 <max.laura95@gmail.com>
2024-12-12 20:35:54 -0500

    como modificar imagenes y audios

---
+++ Dia5/AUDIO/main.py
@@ -0,0 +1,9 @@
+from gtts import gTTS
+import os
+
+texto = input ('Escribe un texto a convertir :')
+tts = gTTS(text=texto,lang='es')
+filename = 'textoconvertido.mp3'
+tts.save(filename)
+
+os.system(f'start {filename}')
